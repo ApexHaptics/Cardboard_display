86d9cfcd98f1f762f33f28d031b866282511a89f
jadentom <jadenttom@gmail.com>
2017-03-05 17:13:05 -0500

    Fixed missing semicolon in getLastPacket

--- app/src/main/java/io/github/apexhaptics/apexhapticsdisplay/BluetoothService.java
+++ app/src/main/java/io/github/apexhaptics/apexhapticsdisplay/BluetoothService.java
@@ -107,7 +107,7 @@ public class BluetoothService {
     }
 
     public BluetoothDataPacket getLastPacket() {
-        return receivedData.poll()
+        return receivedData.poll();
     }
 
     /**
